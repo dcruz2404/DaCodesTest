1936c5cb66e085b19d89fdcec0272f4c0086ca04
dcruz2404 <jdcruz2411@gmail.com>
2020-06-10 21:19:25 -0500

    Add files via upload

---
+++ LogicTest/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LogicTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Datos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(139, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ingrese los datos a evaluar. ";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(79, 79);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(288, 20);
+            this.txtInput.TabIndex = 5;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(79, 124);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(288, 20);
+            this.txtOutput.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resultado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ejemplo: (1-2),(1-4).. ,(N-M) donde N=filas y M= columnas.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 216);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LogicTest/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogicTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calcular()
+        {
+            try
+            {
+                List<string> gridsList = txtInput.Text.Split(',').ToList();
+                int testCases = gridsList.Count;
+                int N = 0;
+                int M = 0;
+                string Output = "";
+                for (int i = 0; i < testCases; i++)
+                {
+                    string grid = gridsList[i].Replace('(',' ').Replace(')',' ').Trim();
+                    N = Convert.ToInt32(grid.Split('-').ToList()[0]);
+                    M = Convert.ToInt32(grid.Split('-').ToList()[1]);
+
+                    if (N == M && N > 1)
+                    {
+                        if (N % 2 == 0)
+                            Output += "L,";
+                        else
+                            Output += "R,";
+                    }
+                    else if (N > M && M > 1)
+                    {
+                        if (M % 2 == 0)
+                            Output += "U,";
+                        else
+                            Output += "D,";
+                    }
+                    else if (M > N)
+                    {
+                        if (N % 2 == 0)
+                            Output += "L,";
+                        else
+                            Output += "R,";
+                    }
+                    else if (M == 1)
+                    {
+                        if (N == 1)
+                            Output += "R,";
+                        else
+                            Output += "D,";
+                    }
+                }
+                txtOutput.Text = Output.TrimEnd(',');
+            }
+            catch (Exception ex)
+            { }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calcular();
+        }
+    }
+}
